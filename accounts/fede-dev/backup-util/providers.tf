f23e3a4669872d70fdb6e7efe884309baa93acd7
Federico Lancerin <federico.lancerin@gmail.com>
2026-05-15 08:49:03 +0200

    move to use specific aws profile per account

--- accounts/fede-dev/backup-util/providers.tf
+++ accounts/fede-dev/backup-util/providers.tf
@@ -3,19 +3,21 @@ terraform {
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.93.0"
     }
   }
 
   backend "s3" {
-    bucket = "tf-infrastructure-fede-dev-389210"
-    key = "accounts/fede-dev/backup-util.tfstate"
-    region = "eu-north-1"
+    bucket       = "tf-infrastructure-fede-dev-389210"
+    key          = "accounts/fede-dev/backup-util.tfstate"
+    region       = "eu-north-1"
     use_lockfile = true
+    profile      = "fede-dev"
   }
 }
 
 provider "aws" {
-  region = "eu-north-1"
+  region  = "eu-north-1"
+  profile = "fede-dev"
 }
