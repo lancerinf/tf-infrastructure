d65212e71547f8d1d887b2aa6a8c13ce484be49b
Federico Lancerin <federico.lancerin@gmail.com>
2026-07-04 18:08:41 +0200

    remove backup-utility user from fede-dev

--- accounts/fede-dev/backup-util/main.tf
+++ accounts/fede-dev/backup-util/main.tf
@@ -2,31 +2,31 @@ resource "aws_s3_bucket" "backup-util-fede-dev-20250330" {
   bucket = "backup-util-fede-dev-20250330"
 }
 
-resource "aws_iam_user" "backup-util-writer" {
-  name = "backup-util-writer-fedebrick"
-}
+# resource "aws_iam_user" "backup-util-writer" {
+#   name = "backup-util-writer-fedebrick"
+# }
 
-data "aws_iam_policy_document" "backup_util_policy_fedebrick" {
-  statement {
-    effect = "Allow"
-    actions = [ "s3:ListBucket" ]
-    resources = [ aws_s3_bucket.backup-util-fede-dev-20250330.arn ]
-  }
-  statement {
-    effect = "Allow"
-    actions = [
-      "s3:GetObject",
-      "s3:PutObject"
-    ]
-    resources = [
-      "${aws_s3_bucket.backup-util-fede-dev-20250330.arn}/fedebrick/*",
-      "${aws_s3_bucket.backup-util-fede-dev-20250330.arn}"
-    ]
-  }
-}
+# data "aws_iam_policy_document" "backup_util_policy_fedebrick" {
+#   statement {
+#     effect = "Allow"
+#     actions = [ "s3:ListBucket" ]
+#     resources = [ aws_s3_bucket.backup-util-fede-dev-20250330.arn ]
+#   }
+#   statement {
+#     effect = "Allow"
+#     actions = [
+#       "s3:GetObject",
+#       "s3:PutObject"
+#     ]
+#     resources = [
+#       "${aws_s3_bucket.backup-util-fede-dev-20250330.arn}/fedebrick/*",
+#       "${aws_s3_bucket.backup-util-fede-dev-20250330.arn}"
+#     ]
+#   }
+# }
 
-resource "aws_iam_user_policy" "backup_util_policy" {
-  name = "backup_util_policy_fedebrick"
-  user = aws_iam_user.backup-util-writer.name
-  policy = data.aws_iam_policy_document.backup_util_policy_fedebrick.json
-}
+# resource "aws_iam_user_policy" "backup_util_policy" {
+#   name = "backup_util_policy_fedebrick"
+#   user = aws_iam_user.backup-util-writer.name
+#   policy = data.aws_iam_policy_document.backup_util_policy_fedebrick.json
+# }
